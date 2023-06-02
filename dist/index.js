0e65a6dc5b274df87ab855b25ea6ebe21263eec0
AK <akrivit@gmail.com>
2023-06-01 23:04:42 -0400

    first pass

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+async function main() {
+    console.log('Hello World!');
+}
+exports.default = main;
+main();
+//# sourceMappingURL=index.js.map
